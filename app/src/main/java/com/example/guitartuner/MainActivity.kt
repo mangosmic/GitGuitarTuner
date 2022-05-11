92ee43793ebe50171b2470b1c1550f733eacdd99
Gosia Jeziorek <malgorzatajeziorek810@gmail.com>
2022-05-11 20:15:23 +0200

    working version

--- app/src/main/java/com/example/guitartuner/MainActivity.kt
+++ app/src/main/java/com/example/guitartuner/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.example.guitartuner
-import android.content.ContextWrapper
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
+import kotlinx.android.synthetic.main.activity_main.* //pozwala korzystac ze wszystkich activity typu button itp bez koniecznosci tworzenia zmiennych
 import android.widget.Button
-import androidx.activity.contextaware.ContextAware
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import java.io.File
-import java.io.PrintWriter
-import java.util.jar.Manifest
-
 
 class MainActivity : AppCompatActivity() {
     //lateinit - pozwala uniknąć inizjowania właściwości gdy kontruujemy obiekt
@@ -24,44 +18,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //define path for record file
-        var recording_path = Environment.getExternalStorageDirectory().toString()+"/record.3gp"
+//        var recording_path = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
         recorder = MediaRecorder()
 
-
-
-        //define button to use them
-        val button = findViewById<Button>(R.id.button)
-        val button2 = findViewById<Button>(R.id.button2)
-        val button3 = findViewById<Button>(R.id.button3)
+        val recording_path = "${externalCacheDir?.absolutePath}/audiorecordtest.3gp"
 
         //turn off button if user dont give permission
-        button.isEnabled = false //start
-        button2.isEnabled = false //stop
+        StartButton.isEnabled = false //start
+        StopButton.isEnabled = false //stop
 
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE),111)
-            button.isEnabled = true //start button on if user give permission
+        StartButton.isEnabled = true //start button on if user give permission
 
         //if user click on start button - start recording
-        button.setOnClickListener{
+        StartButton.setOnClickListener{
             recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
             recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
             recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
             recorder.setOutputFile(recording_path)
             recorder.prepare()
             recorder.start()
-            button2.isEnabled = true
-            button.isEnabled = false
+            StopButton.isEnabled = true
+            StartButton.isEnabled = false
         }
         //if user click on stop button - stop recording
-        button2.setOnClickListener{
+        StopButton.setOnClickListener{
             recorder.stop()
-            button.isEnabled = true
-            button2.isEnabled = false
+            StartButton.isEnabled = true
+            StopButton.isEnabled = false
         }
         //if user click on play button - play recording
-        button3.setOnClickListener{
-            var player = MediaPlayer()
+        PlayButton.setOnClickListener{
+            val player = MediaPlayer()
             player.setDataSource(recording_path)
             player.prepare()
             player.start()
@@ -73,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        val button = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.StartButton)
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
             button.isEnabled = true
